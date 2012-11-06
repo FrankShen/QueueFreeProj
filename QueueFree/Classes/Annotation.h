9a6854fd013c7e49b9ca56a8a841ee8cec247bb2
pursue_ct <truestyle2005@163.com>
2012-11-06 20:31:10 +0800

    Revert "Revert "NearSearch 重构，表示Git的Merge把我恶心到了""
    This reverts commit e7c5b1c1facde780255b9e74525049b05131c44b.

---
+++ QueueFree/Classes/Annotation.h
@@ -0,0 +1,27 @@
+//
+//  AnnotationObject.h
+//  QueueFree
+//
+//  Created by pursue_ct on 12-11-6.
+//  Copyright (c) 2012年 BuG.BS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface Annotation : NSObject<MKAnnotation>
+{
+    CLLocationCoordinate2D coordinate;
+    NSString *subtitle;
+    NSString *title;
+}
+
+@property (nonatomic,readonly) CLLocationCoordinate2D coordinate;
+@property (nonatomic,retain) NSString *subtitle;
+@property (nonatomic,retain) NSString *title;
+
+-(id) initWithCoords:(CLLocationCoordinate2D) coords;
+
+
+@end
---
+++ QueueFree/Classes/NSTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSTableViewController.h
+//  QueueFree
+//
+//  Created by pursue_ct on 12-11-6.
+//  Copyright (c) 2012年 BuG.BS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSTableViewController : UITableViewController
+
+@end
