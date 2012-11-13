73003284ad2d49dc3dd000b8757d8097f140c4ff
pursue_ct <truestyle2005@163.com>
2012-11-14 02:50:20 +0800

    删除冗余代码

--- QueueFree/Classes/NearSearchListViewController.m
+++ QueueFree/Classes/NearSearchListViewController.m
@@ -16,16 +16,6 @@
 
 @implementation NearSearchListViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
